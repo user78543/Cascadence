af97c604610e61a834ef02aa5a0b3e8144599db5
example <email@example.com>
2025-02-11 19:02:02 -0800

    cleanup

--- static/script.js
+++ static/script.js
@@ -3,12 +3,4 @@ window.onload = function () {
     var video = document.getElementById("vid-bg");
     video.muted = true;
     video.play();
-    // video.load();
-    // video.onloadeddata = function () {
-    //     video.play();
-    // };
-    // video.oncanplaythrough = function () {
-    //     console.log("testing");
-    //     video.play();
-    // };
 }
